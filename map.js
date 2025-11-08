25f527d0386e67014892dd73d299c97955149625
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2025-11-08 10:14:47 -0600

    Update map.js

--- map.js
+++ map.js
@@ -1,13 +1,16 @@
-var map = L.map('map').setView([46.7296, -94.6859], 6); // Center on Minnesota
+var map = L.map('map').setView([44.95, -93.1], 4); // Centered on U.S. with zoom level 4
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap'
+    attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-L.marker([45.5571, -94.1624]).addTo(map) // St. Cloud (Storm Chasing)
-    .bindPopup('Storm Chasing Website - St. Cloud');
-L.marker([44.9778, -93.2650]).addTo(map) // Minneapolis (Travel Agency)
-    .bindPopup('Travel Agency Booking Site');
-L.marker([46.7833, -92.1065]).addTo(map) // Duluth (Calendar App)
-    .bindPopup('Marlene’s Calendar App');
-L.marker([45.0087, -95.0456]).addTo(map) // Willmar (Wind Turbine)
-    .bindPopup('Wind Turbine Checklist');
+// Markers for project locations
+L.marker([45.5571, -94.1624]).addTo(map) // St. Cloud, MN (Storm Chasing, Echoes of the Land)
+    .bindPopup('Storm Chasing Website & Echoes of the Land')
+    .on('mouseover', function (e) { this.openPopup(); })
+    .on('mouseout', function (e) { this.closePopup(); });
+
+L.marker([38.2462, 22.0748]).addTo(map) // Distomo, Greece (Distomo Resilience)
+    .bindPopup('The Distomo Resilience Project')
+    .on('mouseover', function (e) { this.openPopup(); })
+    .on('mouseout', function (e) { this.closePopup(); });
